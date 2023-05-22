39b5fd71f9643adb5ae8dcd12b83b772e73ca5a6
herve <herveperel@gmail.com>
2023-05-22 11:20:22 +0200

    bdd

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -1,29 +1,28 @@
 <?php
 
-// src/Controller/ProgramController.php
-
 namespace App\Controller;
-
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
-
 {
-
-    #[Route('/program/', name: 'program_index')]
+    #[Route('/', name: 'index')]
     public function index(): Response
-
     {
-
         return $this->render('program/index.html.twig', [
-
             'website' => 'Wild Series',
-     
-         ]);
-
+        ]);
     }
 
+    #[Route('/{id}', methods: ['GET'], requirements: ['page' => '\d+'], name: 'show')]
+    public function show($id): Response
+    {
+        if(!is_numeric($id)) {
+            throw $this->createNotFoundException();
+        }
+        return $this->render('program/show.html.twig', ['id' => $id]);
+    }
 }
